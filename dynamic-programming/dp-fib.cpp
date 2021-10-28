b5d4633ca01953c0512c91e44ea07786450ab080
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2021-10-28 23:43:57 +0530

    Added the DP approach solution to Fibonacci problem

--- dynamic-programming/dp-fib.cpp
+++ dynamic-programming/dp-fib.cpp
@@ -1,7 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std; 
 
+unordered_map<int, long long> memo = {};
+
+long long fib(int n){
+    if(n <= 2) return 1;
+
+    auto it = memo.find(n); // find if present fib call is already called once and stored in memo map
+    if(it != memo.end()) return it->second;
+    else {
+        long long temp = fib(n-1) + fib(n-2);
+        memo.insert({ n, temp }); // inserting the present call in memo so that it can be reused
+        return temp;
+    }
+}
+
 int main() 
 {
-    
+    cout<<fib(8);
 }
