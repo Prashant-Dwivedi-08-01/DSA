c6caab2a0dce61228c03e0fbbddb36dd1e7599dd
Prashant Dwivedi <prashantdwivedi194@gmail.com>
2022-02-12 11:17:29 +0530

    Improved the comments in stock span problem

--- stack/code/stock_span_problem.py
+++ stack/code/stock_span_problem.py
@@ -3,7 +3,8 @@
 # Eg: [100,80,60,70,60,75,85] then for 75 it would be (60, 70, 60, 75) i.e 4 days
 
 # SOLUTION: Here, we need to go back till we find the first greater element because then 
-# actually we will be doing the same thing, i.e. covering the consecutive smaller numbers than present
+# actually we will be doing the same thing, i.e. covering the consecutive smaller numbers 
+# than present
 
 # Here, we are storing index of days array as ans to the problem of storing the first greater
 # and then we simply substracting the index with ans array's index
